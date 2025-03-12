a7038b9e71ae81651b41cfe688d265271b313ab5
Tig3r <fox.98@live.it>
2025-03-12 18:44:26 +0100

    Remove depend on VelocityControl jar using reflections

--- src/main/java/com/james090500/VelocityGUI/helpers/ChatControlHelper.java
+++ src/main/java/com/james090500/VelocityGUI/helpers/ChatControlHelper.java
@@ -1,32 +1,45 @@
 package com.james090500.VelocityGUI.helpers;
 
 import com.velocitypowered.api.proxy.Player;
-import org.mineacademy.velocitycontrol.SyncedCache;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ChatControlHelper {
 
-    public static String getNick(Player player) {
-        if(!doesClassExist("org.mineacademy.velocitycontrol.SyncedCache")) {
-            return null;
-        }
+    static {
+        try {
 
-        String nickname = SyncedCache.fromName(player.getUsername()).getNick();
-        return nickname != null ? nickname : player.getUsername();
+            Class<?> syncedCache = Class.forName("org.mineacademy.chatcontrol.SyncedCache");
+
+            Method fromPlayerName = syncedCache.getMethod("fromPlayerName", String.class);
+            fromPlayerName.setAccessible(true);
+            ChatControlHelper.fromPlayerName = fromPlayerName;
+
+            Method getNick = syncedCache.getMethod("getNameOrNickColorless");
+            getNick.setAccessible(true);
+            ChatControlHelper.getNick = getNick;
+
+            loaded = true;
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            loaded = false;
+        }
     }
 
-    /**
-     * Checks if a class exists or not
-     * @param name
-     * @return
-     */
-    private static boolean doesClassExist(String name) {
+    private static Method fromPlayerName;
+    private static Method getNick;
+    private static boolean loaded;
+
+    public static String getNick(Player player) {
+        if(!loaded)
+            return null;
         try {
-            Class c = Class.forName(name);
-            if (c != null) {
-                return true;
-            }
-        } catch (ClassNotFoundException e) {}
-        return false;
+            Object syncedCache = fromPlayerName.invoke(null, player.getUsername());
+            String nickName = (String) getNick.invoke(syncedCache);
+            return nickName != null ? nickName : player.getUsername();
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
